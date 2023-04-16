a515c86d1beb082bcd595bd3d8b70b71a62b055f
Romano Roth <romano.roth@gmx.ch>
2023-04-16 10:38:35 +0200

    Initial commit of code of video 1-12

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void homeResponse() {
+		String body = this.restTemplate.getForObject("/", String.class);
+		Assertions.assertThat(body).contains("Spring is here!");
+	}
+}
